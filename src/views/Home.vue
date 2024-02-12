5257896c94a7db82536f47cf259d285479cf86b3
liujinxuan <1256485667@qq.com>
2024-02-12 10:56:44 +0800

    init project by xuan

---
+++ src/views/Home.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="home-view">
+    <div class="main">
+      <div class="plate-1">
+        <div class="banner">
+          <svg
+            class="bigBanner"
+            xmlns="http://www.w3.org/2000/svg"
+            width="500"
+            height="300"
+            style="font-style: no"
+          >
+            <text
+              x="50%"
+              y="50%"
+              dominant-baseline="middle"
+              text-anchor="middle"
+              font-size="100px"
+              fill="#fff"
+            >
+              🐟
+            </text>
+          </svg>
+        </div>
+        <div class="text-center">
+          <button
+            type="button"
+            class="btn createNftBtn"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
+            Create NFTs
+          </button>
+        </div>
+        <div class="content">
+          <small>
+            我是教學案例，請同學們不要直接用該代碼作為自己項目，至少把樣式全部改一遍再用
+          </small>
+        </div>
+        <p>1.做路由</p>
+        <p>2.創建頁面</p>
+        <p>3.js邏輯文件創建</p>
+        <p>4.合約地址文件創建</p>
+        <p>5.頁面變量創建</p>
+        <p>6.頁面變量獲得的func創建 - 頁面需要的合約實例創建</p>
+      </div>
+    </div>
+
+    <CreateNFTsPopup />
+  </div>
+</template>
+
+<script>
+const CreateNFTsPopup = () => import('@/components/Popups/CreateNFTsPopup');
+import home from '@/mixins/home.js';
+export default {
+  mixins: [home],
+  data() {
+    return {};
+  },
+  name: 'Home',
+  methods: {
+    goOtherPage(url) {
+      window.open(url);
+    },
+  },
+  components: {
+    CreateNFTsPopup,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.createNftBtn {
+  max-width: 500px;
+  height: 60px;
+  width: 90%;
+  margin: 0 auto;
+  color: #000 !important;
+  border: 1px solid #000;
+  font-size: 26px;
+  // font-weight: bolder;
+  &:disabled {
+    opacity: 0.4;
+  }
+
+  &:hover {
+    background-color: $clrYellow;
+    border: 1px solid $clrYellow;
+    color: #000 !important;
+  }
+}
+</style>
---
+++ src/views/Stake.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="stake-view">
+    <div class="container mini">
+      <h1>質押</h1>
+      <div>
+        <div>
+          <div>
+            <p>sFISH 价格 ｜ {{ SFish_USDPrice }}</p>
+            <p>价值 ｜ 1個 sFISH = {{ Fish_SFishPrice }} 個FISH</p>
+            <p>你的 FISH 余额 ｜ {{ FISHBalanceOf }} （FISH）</p>
+            <p>你已质押 sFISH ｜ {{ SFISHBalanceOf }} （sFISH）</p>
+          </div>
+
+          <div>
+            <div>
+              <div>
+                <div class="row">
+                  <div class="col-6">
+                    <div
+                      type="button"
+                      @click="reverse()"
+                      :class="!stakeState ? '' : 'action'"
+                    >
+                      質押
+                    </div>
+                  </div>
+                  <div class="col-6">
+                    <div
+                      type="button"
+                      @click="reverse()"
+                      :class="stakeState ? '' : 'action'"
+                    >
+                      贖回
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div class="row">
+                  <div class="col text-start stake-left-table-thead">
+                    {{ stakeState ? 'FISH Balance' : 'sFISH Balance' }}
+                  </div>
+                  <div class="col text-end">
+                    {{ stakeState ? FISHBalanceOf : SFISHBalanceOf }}
+                  </div>
+                </div>
+                <div class="input-group mb-3" v-if="stakeState">
+                  <span class="input-group-text" id="inputGroup-sizing-default"
+                    >Fish</span
+                  >
+                  <input
+                    type="number"
+                    class="form-control"
+                    v-model="fishInput"
+                  />
+                </div>
+                <div class="input-group mb-3" v-else>
+                  <span class="input-group-text" id="inputGroup-sizing-default"
+                    >sFish</span
+                  >
+                  <input
+                    type="number"
+                    class="form-control"
+                    v-model="sFishInput"
+                  />
+                </div>
+              </div>
+
+              <button
+                type="button"
+                class="btn btn-warning stake-btn"
+                @click="apporve()"
+                v-if="
+                  (stakeState && !fishIsApporve) ||
+                  (!stakeState && !sFishIsApporve)
+                "
+              >
+                APPROVE
+              </button>
+              <button
+                type="button"
+                class="btn btn-warning stake-btn"
+                @click="stake()"
+                v-if="fishIsApporve && stakeState"
+              >
+                質押
+              </button>
+              <button
+                type="button"
+                class="btn btn-warning stake-btn"
+                @click="unStake()"
+                v-if="sFishIsApporve && !stakeState"
+              >
+                贖回
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const TokenIcon = () => import('@/components/UiComponents/TokenIcon');
+import stakeMixin from '@/mixins/stake.js';
+export default {
+  mixins: [stakeMixin],
+  components: {
+    TokenIcon,
+  },
+  data() {
+    return {
+      connectLoader: 0,
+      cefreshLoader: false,
+    };
+  },
+  computed: {
+    pools() {
+      return this.$store.getters.getPools;
+    },
+  },
+  created() {
+    const isConnected = this.$store.getters.getWalletIsConnected;
+
+    if (!isConnected) {
+      this.$router.push({ name: 'home' });
+      alert('請先連結錢包');
+      return false;
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.action {
+  background-color: #ffc107;
+  color: #000;
+}
+</style>
